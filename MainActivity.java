0da8ce359fec7da250bd612fe3b78b1df994e5b6
DanOS2015 <dan3601.dos@gmail.com>
2016-07-22 12:02:53 +0100

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -12,6 +12,7 @@ import android.view.View.OnClickListener;
 
 public class MainActivity extends AppCompatActivity implements OnClickListener
 {
+    //button and edittext created in xml 
     Button done;
     EditText input;
 
@@ -20,19 +21,25 @@ public class MainActivity extends AppCompatActivity implements OnClickListener
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //find button and edittext by finding their ids
         done = (Button) findViewById(R.id.done);
         input = (EditText) findViewById(R.id.numOfSubjects);
+        //set on click listener on the button 
         done.setOnClickListener((OnClickListener)this);
     }
 
     @Override
     public void onClick(View v)
     {
+        //if the text field is empty and button is clicked do nothing 
         if(TextUtils.isEmpty(input.getText().toString()))
             return;
+        //get value from edittext and covert it to an integer 
         int numOfSubjects = Integer.parseInt(input.getText().toString());
         Intent i = new Intent(this, Subjects.class);
+        //send numOfSubjects to next activity
         i.putExtra("result", numOfSubjects);
+        //start next activity 
         startActivity(i);
     }
 }
