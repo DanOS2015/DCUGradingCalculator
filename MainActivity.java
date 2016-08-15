fe9183e7625aa6a3b3bc086e4754531c1dd98578
Daniel O'Sullivan <dan3601.dos@gmail.com>
2016-08-15 18:56:24 +0100

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -14,7 +14,8 @@ public class MainActivity extends AppCompatActivity implements OnClickListener
 {
     //button, edittext and textview created in xml 
     Button done;
-    EditText input;
+    Edittext subInput;
+    EditText credInput;
     TextView error;
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -23,7 +24,8 @@ public class MainActivity extends AppCompatActivity implements OnClickListener
         setContentView(R.layout.activity_main);
         //find button, edittext and textview by finding their ids
         done = (Button) findViewById(R.id.done);
-        input = (EditText) findViewById(R.id.numOfSubjects);
+        subInput = (EditText) findViewById(R.id.numOfSubjects);
+        credInput = (EditText) findViewById(R.id.numOfCredits);
         error = (TextView) findViewById(R.id.errorMessage);
         //set on click listener on the button 
         done.setOnClickListener((OnClickListener)this);
@@ -32,19 +34,29 @@ public class MainActivity extends AppCompatActivity implements OnClickListener
     @Override
     public void onClick(View v)
     {
-        //if the text field is empty and button is clicked set message and do nothing 
-        if(TextUtils.isEmpty(input.getText().toString()))
+        //try and catch to handle any exceptions
+        try
         {
-            error.setText("ERROR: Please enter a number before continuing");
-            return;
+            //if the text field is empty and button is clicked set message and do nothing 
+            if(TextUtils.isEmpty(input.getText().toString()))
+            {
+                error.setText("ERROR: Please enter a number before continuing");
+                return;
+            }
+            //get value from edittext and covert it to an integer 
+            int numOfSubjects = Integer.parseInt(subInput.getText().toString());
+            int numOfCredits = Integer.parseInt(credInput.getText().toString());
+            Intent i = new Intent(this, Subjects.class);
+            //send numOfSubjects and numOfCredits to next activity
+            i.putExtra("result", numOfSubjects);
+            i.putExtra("credits", numOfCredits);
+            //start next activity 
+            startActivity(i);
         }
-        //get value from edittext and covert it to an integer 
-        int numOfSubjects = Integer.parseInt(input.getText().toString());
-        Intent i = new Intent(this, Subjects.class);
-        //send numOfSubjects to next activity
-        i.putExtra("result", numOfSubjects);
-        //start next activity 
-        startActivity(i);
+        catch(Exception e)
+        {
+            error.setText("ERROR: Exception caught, please review validity of inputs or please restart the application");
+        }
     }
 }
 
